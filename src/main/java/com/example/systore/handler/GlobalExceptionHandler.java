abe96e081d1e55d60e36f7bfce87d93988ee825e
H-SChan <dlwp7581@gmail.com>
2022-04-25 12:15:17 +0900

    [add] exception handler

---
+++ src/main/java/com/example/systore/handler/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.example.systore.handler;
+
+import com.example.systore.domain.dto.response.Response;
+import com.example.systore.domain.dto.response.ResponseEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public Response illegalArgumentExceptionHandler(IllegalArgumentException e) {
+        log.error("httpClientErrorExceptionHandler()");
+        log.error(e.getMessage());
+        return new Response(HttpStatus.NOT_FOUND, e.getMessage());
+    }
+
+}
