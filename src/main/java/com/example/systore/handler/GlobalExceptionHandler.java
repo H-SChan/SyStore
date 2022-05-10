d4412cf4c489a48703068a0542304e5f05a80a23
H-SChan <dlwp7581@gmail.com>
2022-05-10 09:46:33 +0900

    [add] null point exception handler

--- src/main/java/com/example/systore/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/systore/handler/GlobalExceptionHandler.java
@@ -12,9 +12,16 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public Response illegalArgumentExceptionHandler(IllegalArgumentException e) {
-        log.error("httpClientErrorExceptionHandler()");
+        log.error("illegalArgumentExceptionHandler()");
         log.error(e.getMessage());
         return new Response(HttpStatus.NOT_FOUND.value(), e.getMessage());
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    public Response nullPointerExceptionHandler(NullPointerException e) {
+        log.error("NullPointerExceptionHandler()");
+        log.error(e.getMessage());
+        return new Response(HttpStatus.INTERNAL_SERVER_ERROR.value(), "서버 오류 발생");
     }
 
 }
